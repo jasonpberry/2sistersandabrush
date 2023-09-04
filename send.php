cd18cd98944410d6b7a77077414619702de6181a
Jason Berry <jasonpberry78@gmail.com>
2023-09-04 12:34:21 -0400

    nocase: twilio sms work.

--- send.php
+++ send.php
@@ -9,20 +9,22 @@ error_reporting(E_ALL);
 require __DIR__ . '/libs/src/Twilio/autoload.php';
 
 // Your Account SID and Auth Token from console.twilio.com
-$sid = "ACXXXXXX";
-$token = "YYYYYY";
+$sid = "ACb0a1ce542b61353c4d890ff7f47d7f0a";
+$token = "93ccb648f3daded5ac299b1995455aac";
 $client = new Twilio\Rest\Client($sid, $token);
 
 // Use the Client to make requests to the Twilio REST API
-$client->messages->create(
+$message = $client->messages->create(
     // The number you'd like to send the message to
     '+14438072661',
     [
         // A Twilio phone number you purchased at https://console.twilio.com
-        'from' => '+15017250604',
+        'from' => '++18339954202',
         // The body of the text message you'd like to send
-        'body' => "Hey Jenny! Good luck on the bar exam!",
+        'body' => "how long from 12:34 does this take?",
     ]
 );
 
-print_r($client);
+echo "<pre>";
+print_r($message->sid);
+echo "</pre>";
