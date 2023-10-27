a8acb3d6491f2772889bc11f98a33849241f5824
Jason Berry <jasonpberry78@gmail.com>
2023-10-27 16:50:56 -0400

    nocase: re-commit after remove cache efforts.

--- cms/lib/menus/default/view_functions.js
+++
@@ -1,6 +0,0 @@
-
-function viewCancel() {
-  if ($('#returnUrl').val()) { self.location = $('#returnUrl').val(); }
-  else                       { self.location = '?menu=' + $('#menu').val(); }
-}
-
